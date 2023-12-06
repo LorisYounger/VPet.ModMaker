fb81811be05ac025c0953048bfb206dd436d1071
Hakoyu <Hakoyu@outlook.com>
2023-12-06 13:11:25 +0800

    修复资源引用错误问题

---
+++ VPet.ModMaker/Models/ObservablePoint.cs
@@ -0,0 +1,31 @@
+﻿using HKW.HKWUtils.Observable;
+
+namespace VPet.ModMaker.Models;
+
+public class ObservablePoint<T>
+{
+    public ObservableValue<T> X { get; } = new();
+    public ObservableValue<T> Y { get; } = new();
+
+    public ObservablePoint() { }
+
+    public ObservablePoint(T x, T y)
+    {
+        X.Value = x;
+        Y.Value = y;
+    }
+
+    public void SetValue(T x, T y)
+    {
+        X.Value = x;
+        Y.Value = y;
+    }
+
+    public ObservablePoint<T> Copy()
+    {
+        var result = new ObservablePoint<T>();
+        result.X.Value = X.Value;
+        result.Y.Value = Y.Value;
+        return result;
+    }
+}
---
+++ VPet.ModMaker/Models/ObservableRect.cs
@@ -0,0 +1,39 @@
+﻿using HKW.HKWUtils.Observable;
+
+namespace VPet.ModMaker.Models;
+
+public class ObservableRect<T>
+{
+    public ObservableValue<T> X { get; } = new();
+    public ObservableValue<T> Y { get; } = new();
+    public ObservableValue<T> Width { get; } = new();
+    public ObservableValue<T> Height { get; } = new();
+
+    public ObservableRect() { }
+
+    public ObservableRect(T x, T y, T width, T hetght)
+    {
+        X.Value = x;
+        Y.Value = y;
+        Width.Value = width;
+        Height.Value = hetght;
+    }
+
+    public void SetValue(T x, T y, T width, T hetght)
+    {
+        X.Value = x;
+        Y.Value = y;
+        Width.Value = width;
+        Height.Value = hetght;
+    }
+
+    public ObservableRect<T> Copy()
+    {
+        var result = new ObservableRect<T>();
+        result.X.Value = X.Value;
+        result.Y.Value = Y.Value;
+        result.Width.Value = Width.Value;
+        result.Height.Value = Height.Value;
+        return result;
+    }
+}
